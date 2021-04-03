cbfbd5ad63de4173f94989e82b3aa7df3ede4bd6
VonnegutFcox <levifcox@gmail.com>
2021-04-03 15:28:59 +0300

    db add

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,9 +6,17 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField(
+        verbose_name='Заголовок',
+        max_length=200
+    )
+    slug = models.SlugField(
+        verbose_name='Адрес для страницы',
+        unique=True
+    )
+    description = models.TextField(
+        verbose_name='Описание'
+    )
 
     def __str__(self):
         return f"{self.title}"
@@ -18,12 +26,18 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User,
+                               verbose_name='Автор',
                                on_delete=models.CASCADE,
                                related_name="posts")
     group = models.ForeignKey(Group,
+                              verbose_name='Группа',
                               on_delete=models.SET_NULL,
                               related_name="posts",
                               blank=True, null=True)
 
     class Meta:
         ordering = ['-pub_date']
+
+    @classmethod
+    def __str__(cls):
+        return cls.text[:15]
